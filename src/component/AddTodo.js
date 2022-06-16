ffec8a4d831f07091b0d9066403ceaba113aef73
KimberlySinchi <kimberlytoledo36@gmail.com>
2022-06-15 20:28:59 -0400

    Initial assignment 2 commit

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -7,6 +7,7 @@ class AddTodo extends Component {
     super();
     this.state = {
       content: "",
+      date: ""  // Key named data with an initial value of an empty string
     };
   }
   // The handleChange function updates the react state with the new input value provided from the user.
@@ -15,6 +16,7 @@ class AddTodo extends Component {
   handleChange = (event) => {
     this.setState({
       content: event.target.value,
+      date: Date().toLocaleString('en-US')  // Updating the key data
     });
   };
   // The handleSubmit function collects the forms input and puts it into the react state.
@@ -27,6 +29,7 @@ class AddTodo extends Component {
       this.props.addTodo(this.state);
       this.setState({
         content: "",
+        data: ""
       });
     }
   };
